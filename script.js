2fdf22b5d75cb56ceb983dd48b65c32c67d23b54
Iraiore <yagoiraiore@gmail.com>
2023-09-10 21:22:15 -0400

    atualziando os icones do cabecalho

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function menuShow(){
     let menuMobile = document.querySelector('.mobile-menu');
     if(menuMobile.classList.contains('open')){
        menuMobile.classList.remove('open');
-       document.querySelector('.icon').src = "image/menu_white_36dp.svg"
+       document.querySelector('.icon').src = "image/icon/menu_white_36dp.svg"
     }else{
        menuMobile.classList.add('open');
-       document.querySelector('.icon').src = "image/close_white_36dp.svg" 
+       document.querySelector('.icon').src = "image/icon/close_white_36dp.svg" 
     }
 }
